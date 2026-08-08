491cf01a188c8ffdb8deb0584f0fd6ba8bcef0e0
Gabriel De Sixto <gabrielsolanodesixto@gmail.com>
2026-08-07 21:43:33 -0600

    feat: update for my portfolio

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,12 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['"Geist Sans"', 'system-ui', 'sans-serif'],
+				mono: ['"Geist Mono"', 'monospace'],
+			},
+		},
 	},
 	plugins: [],
 }
